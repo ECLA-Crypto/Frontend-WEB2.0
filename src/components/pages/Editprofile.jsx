d08bb97830be6590fcf91c29701c21efa3c21f97
Kelvin Ajayi <68742854+Kelvide@users.noreply.github.com>
2022-09-20 20:12:44 +0100

    Update Editprofile.jsx

--- src/components/pages/Editprofile.jsx
+++ src/components/pages/Editprofile.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useSnackbar } from "notistack";
+import { useState } from "react";
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -7,12 +8,16 @@ import CommerceNav from "../static/CommerceSection/CommerceNav"
 
 const Editprofile = ({walletAddress}) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const profileUsername = useSelector(state=>state.profileUsername)
   const profileEmail = useSelector(state=>state.profileEmail)
   const profileWalletAddress = useSelector(state=>state.profileWalletAddress)
   const profileImage = useSelector(state=>state.profileImage)
   const { enqueueSnackbar } = useSnackbar();
+  const [usernameMessage, setUsernameMessage] = useState(false)
+  const [emailMessage, setEmailMessage] = useState(false)
+  const [updatedUsername, setUpdatedUsername] = useState(true)
+  const [updatedEmail, setUpdatedEmail] = useState(true)
   useEffect(() => {
     axios.post('https://ecla-backend.vercel.app/api/user/findWallet',{walletAddress}).then(res=>{
       if (res.data.status) {
@@ -26,6 +31,59 @@ const Editprofile = ({walletAddress}) => {
       }
     })
   }, [walletAddress,enqueueSnackbar,dispatch, navigate])
+  // Check if username is existing in the db
+  const updateUsername = (e) => {          
+    axios.post('https://ecla-backend.vercel.app/api/user/update/username', { walletAddress,username:profileUsername }).then((res)=>{
+      console.log(res.data)
+      if (res.data.status) {
+        setUpdatedUsername(true)
+      } else {
+        setUpdatedUsername(false)
+        setUsernameMessage(`${res.data.message}`)
+      } 
+    }).catch(err=>{
+      setUpdatedUsername(false)
+      enqueueSnackbar(`${err.message}`, { variant:"error" });
+    })
+    dispatch({type:"SET_PROFILE_USERNAME", payload:e.target.value})
+  }
+  // Check if email is existing in the db
+  const updateEmail = (e) => {          
+    axios.post('https://ecla-backend.vercel.app/api/user/update/email', { walletAddress,email:profileEmail }).then((res)=>{
+      if (res.data.status) {
+        setUpdatedEmail(true)
+      } else {
+        setUpdatedEmail(false)
+        setEmailMessage(`${res.data.message}`)
+      } 
+    }).catch(err=>{
+      setUpdatedEmail(false)
+      enqueueSnackbar(`${err.message}`, { variant:"error" });
+    })
+    dispatch({type:"SET_PROFILE_EMAIL", payload:e.target.value})
+  }
+  // Edit Button function
+  const editProfile = () => {
+    if (updatedUsername&&updatedEmail) {
+      axios.post('https://ecla-backend.vercel.app/api/user/editprofile', {
+        walletAddress,
+        updateUsername:updatedUsername,
+        updateEmail:updatedEmail,
+        email:profileEmail,
+        username:profileUsername,
+        profileImage
+      }).then((res)=>{
+        if (res.data.status){
+          enqueueSnackbar(`${res.data.message}`, {variant:"success"});
+        } else {
+          enqueueSnackbar(`${res.data.message}`, {variant:"error"});
+        }
+        console.log(res.data)
+      }).catch(err=>{
+        enqueueSnackbar(`${err.message}`, { variant:"error" });
+      })
+    }
+  }
    
   return (
     <div className="w-full min-h-screen bg-gradient-to-tr from-eclaLightSkyBlue/30 to-eclaSkyBlue">
@@ -35,11 +93,17 @@ const Editprofile = ({walletAddress}) => {
       <div className="max_size w-full mt-5">
         <div className="w-fit px-10 bg-white flex flex-col gap-3 py-10 rounded mx-auto my-auto">
           <p className="text-2xl md:text-3xl font-semibold text-center text-black/90">Edit Your Profile</p>
-          <input type="text" value={profileUsername} onChange={(e)=>dispatch({type:"SET_PROFILE_USERNAME", payload:e.target.value})} placeholder="Username" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
-          <input type="email" value={profileEmail} onChange={(e)=>dispatch({type:"SET_PROFILE_EMAIL", payload:e.target.value})}  placeholder="Email Address" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
-          <input type="text" value={profileWalletAddress} disabledn placeholder="WalletAddress" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
-          <input type="text" value={profileImage} disabledn placeholder="Profile Image" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
-          <button className="px-5 py-2 bg-eclaLightSkyBlue rounded mx-auto w-fit text-white">Edit Profile</button>
+          <div>
+            <input type="text" value={profileUsername} onChange={updateUsername} placeholder="Username" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
+            {usernameMessage&&(<p className="text-red-700 text-sm ml-2">{usernameMessage}</p>)}
+          </div>
+          <div>
+            <input type="email" value={profileEmail} onChange={updateEmail}  placeholder="Email Address" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
+            {emailMessage&&(<p className="text-red-700 text-sm ml-2">{emailMessage}</p>)}
+          </div>
+          <input type="text" value={profileWalletAddress} disabled={true} placeholder="WalletAddress" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
+          <input type="text" value={profileImage} onChange={(e)=>dispatch({type:"SET_PROFILE_IMAGE", payload:e.target.value})} placeholder="Profile Image" className="w-full px-3 font-semibold py-2.5 border outline-none border-eclaLightSkyBlue/20 rounded-lg text-xs"/>
+          <button className="px-5 py-2 bg-eclaLightSkyBlue rounded mx-auto w-fit text-white" onClick={editProfile}>Edit Profile</button>
         </div>
       </div>
     </div>
